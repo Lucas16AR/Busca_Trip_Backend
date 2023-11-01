01ca5a991e62cf4ac565aee3d1e6371bd2f6119c
Lucas16AR <luqui0008@gmail.com>
2023-11-01 17:58:12 -0300

    pom

--- trip/src/main/java/com/trip/TripApplication.java
+++ trip/src/main/java/com/trip/TripApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.trip")
 public class TripApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TripApplication.class, args);
 	}
-
 }
