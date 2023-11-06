e373ff5d2928f480484354c757b76ccdc6d2fe35
Lucas16AR <luqui0008@gmail.com>
2023-11-06 18:05:12 -0300

    models

--- trip/src/main/java/com/trip/models/UserModel.java
+++ trip/src/main/java/com/trip/models/UserModel.java
@@ -1,6 +1,13 @@
 package com.trip.models;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.List;
 
